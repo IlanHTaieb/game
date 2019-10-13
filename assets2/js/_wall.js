9c8d4a35c44c84ff42edd11c52b59aa2fe569c96
Ilan Taieb <ilan.taieb@outlook.fr>
2019-10-13 02:33:29 +0200

    remove old

--- assets2/js/_wall.js
+++
@@ -1,8 +0,0 @@
-import {Map} from "./_map";
-
-export const Wall = {
-    init: function(bloc) {
-        $('.bloc[pos-y=' + bloc.posY + '][pos-x=' + bloc.posX + ']').addClass('pos-wall')
-        Map.clear(bloc.id)
-    }
-}

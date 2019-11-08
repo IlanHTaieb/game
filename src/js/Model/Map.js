3ffbb58979b0080b0445dc6281a38b21210b19c5
Ilan Taieb <ilan.taieb@outlook.fr>
2019-11-08 01:57:29 +0100

    wip

--- src/js/Model/Map.js
+++ src/js/Model/Map.js
@@ -6,8 +6,15 @@ export class Map {
     width = 10
     arena = []
     freeBlocs = []
+    characters
+    currentPlayer
 
-    constructor() {
+    constructor(characters) {
+        console.log('- Setting all characters')
+        console.log(characters)
+        console.log('- Nb: ' + (Object.keys(characters).length))
+        this.characters = characters
+
         for (let y = 1; y <= this.height; y++) {
             this.arena[y] = []
 
@@ -72,12 +79,35 @@ export class Map {
         } else {
             this.chooseBlock()
         }
+    }
 
+    setTestFirstPlayer(characters) {
+        let first =  Object.entries(characters)[
+            Math.floor(
+                Math.random() * Object.entries(characters).length
+            )
+            ][1]
+
+        console.log('- The first player is ' + first.getCurrent().getType())
+        this.currentPlayer = first
+
+        return first
+    }
+
+    getCurrentplayer() {
+        return this.characters[this.currentPlayer][1]
+    }
+
+    setCurrentPlayer(character) {
+        this.currentPlayer =
+            this.currentPlayer >= (Object.keys(this.characters).length - 1)
+                ? 0
+                : this.currentPlayer + 1
+
+        character.getCurrent().setRound(val)
     }
 
     move(character, bloc) {
-        console.log('- Starting move')
-
         let currentPosition = {
             Y: character.getCurrent().getPosY(),
             X: character.getCurrent().getPosX()
@@ -93,16 +123,34 @@ export class Map {
             X: destinationPosition.X - currentPosition.X
         }
 
-        console.log('- Character position')
-        console.log(currentPosition)
-        console.log('- Bloc position')
-        console.log(destinationPosition)
-        console.log('- Move length')
-        console.log(move)
 
-        if ((move.Y + move.X) <= character.getCurrent().getMove() && (move.Y + move.X) > -4) {
+        if (this.checkMove(character, move)) {
             this.createFreeBloc(bloc.posY, bloc.posX)
             character.move(bloc.posY, bloc.posX)
+
+            console.log('- Setting played')
+            character.getCurrent().setHasPlayed(true)
+            console.log('- Done')
+        }
+    }
+
+    checkMove(character, move) {
+        console.log('- Setting no played')
+        character.getCurrent().setHasPlayed(false)
+        console.log('- Done')
+
+        let speed = character.getCurrent().getMove()
+        let horizontalMove = move.X <= speed && move.X >= -speed
+        let verticalMove = move.Y <= speed && move.Y >= -speed
+        let bottomRightMove = (move.X + move.Y) <= speed && (move.X + move.Y) >= -speed
+        let toBottomLeftMove = (move.Y + move.X) == 0
+            ? (move.Y == 1 && move.X == -1) || (move.Y == -1 && move.X == 1)
+            : true
+
+        if (horizontalMove && verticalMove && bottomRightMove && toBottomLeftMove) {
+            return true
+        } else  {
+            return false
         }
     }
 
--- src/js/app.js
+++ src/js/app.js
@@ -11,19 +11,17 @@ import {ItemManager} from "./Manager/Elements/ItemManager.js";
 import {Character} from "./Model/Character.js";
 
 
-
 $(document).ready(function () {
     //Router.home()
     Router.arena('Toto', 'Ploufy');
-    let map = new Map();
-    map.render();
 
     let characters = {
         pirate: new CharacterManager(new Pirate()),
         marines: new CharacterManager(new Marines())
     };
 
-    console.log();
+    let map = new Map(characters);
+    map.render();
 
     let knife = new ItemManager(new Knife());
     let shootgun = new ItemManager(new Shootgun());
@@ -39,24 +37,74 @@ $(document).ready(function () {
         map.init(wall)
     }
 
-    let firstPlayer = Object.entries(characters)
-        [Math.floor(Math.random() * Object.entries(characters).length)]
-        [1];
+    let currentPlayer = map.setTestFirstPlayer(characters)
+    let nextPlayer
+
+    //map.move(player, $(this).data())
 
     $('.bloc').click(function () {
-        let currentPlayer = firstPlayer
-
-        for (let [key, player] of Object.entries(characters)) {
-            if (player.getCurrent().getRound()) {
-                currentPlayer = player
-                player.getCurrent().setRound(false)
-            } else {
-                player.getCurrent().setRound(true)
-            }
-        }
-
+        console.log('- Click to move')
         if ($(this).data('type') == 'free' || $(this).data('type') == 'item') {
-            map.move(currentPlayer, $(this).data())
+            $.when(checkRound($(this).data()))
+                .done(function () {
+                    currentPlayer = nextPlayer
+                    console.log('- After done')
+                    console.log('- Next: ' + nextPlayer.getCurrent().getType())
+                    console.log('- Current: ' + currentPlayer.getCurrent().getType())
+                })
         }
     })
+
+    // TODO
+    //console.log('- Setting no played')
+    //character.getCurrent().setHasPlayed(false)
+    //console.log('- Done')
+
+    function checkRound(bloc) {
+        for (let [key, player] of Object.entries(characters)) {
+            if (player === currentPlayer) {
+                map.move(currentPlayer, bloc)
+            } else {
+                console.log('- No current')
+                console.log('- Player: ' + player.getCurrent().getType())
+                console.log('- Current: ' + currentPlayer.getCurrent().getType())
+                console.log('- Current has played: ' + currentPlayer.getCurrent().getHasPlayed())
+                nextPlayer =
+                    currentPlayer.getCurrent().getHasPlayed()
+                        ? player
+                        : currentPlayer
+
+                console.log('- Next: ' + nextPlayer.getCurrent().getType())
+                console.log('- Current: ' + currentPlayer.getCurrent().getType())
+            }
+        }
+    }
+
+
+    /*
+     $('.bloc').click(function () {
+        console.log('- Click to move')
+        if ($(this).data('type') == 'free' || $(this).data('type') == 'item') {
+            console.log('- No obstacles')
+            for (let [key, player] of Object.entries(characters)) {
+                console.log('- Fetch user')
+                if (player.getCurrent().getRound() === true) {
+                    console.log('- Round ok for ' + player.getCurrent().getType())
+                    map.move(player, $(this).data())
+                    map.setCurrentPlayer(player, false)
+                } else {
+                    console.log('- Round not ok for ' + player.getCurrent().getType())
+                    if (map.getLastPlayer() != player.getCurrent().getType()) {
+                        console.log('- ' + player.getCurrent().getType() + ' is not the last player')
+                        if (map.getFirstMoveActed() === true) {
+                            console.log('- First move acted')
+                            player.getCurrent().setRound(true)
+                            map.setCurrentPlayer(player, true)
+                        }
+                    }
+                }
+            }
+        }
+    })
+     */
 });
